4f5766486edbb93b62da7200979425cbf34e1cf8
Lydia Alem <107647071+LydiaAlem@users.noreply.github.com>
2023-07-18 13:15:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ main program
 @description: this script initializes the graphical user interface (GUI) for the program, and
 it imports the necessary modules and calls the 'mainScreen()' function to start the GUI.
 
-Author: Lydia Alem
-Date: July 18, 2023
+@author: Lydia Alem
+@date: July 18, 2023
 '''
 
 import tkinter as tk
